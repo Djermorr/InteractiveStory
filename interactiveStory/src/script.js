8681031f4e07ab78fe076ab9bac1c6bc3301f653
Robbe Van Dijck <robbeelvandijck@gmail.com>
2023-03-30 15:45:17 +0200

    adding node red connection, updating code to follow path, adding ending option

--- interactiveStory/src/script.js
+++ interactiveStory/src/script.js
@@ -1,17 +1,71 @@
 const optionButtons = document.querySelectorAll('#option-buttons button');
 const content = document.querySelector('#content');
 const resetButton = document.querySelector('#reset-button');
+const buttonLeft = document.querySelector('#buttonLeft');
+const buttonRight = document.querySelector('#buttonRight')
+let background = document.querySelector('body')
 
+var currentTime = Date.now();
+var time = currentTime;
+
+/*
+Banaan = B
+Taart = T
+Eten = E
+
+ */
+let currentOption = 'option1';
 const options = {
-  option1: "Je hebt het 1e cadeautje gekozen!",
-  option2: "Je hebt het 2e cadeautje gekozen!",
-  option3: "Je hebt het 3e cadeautje gekozen!",
-  option4: "Je hebt het 4e cadeautje gekozen!",
-  option5: "Je hebt het 5e cadeautje gekozen!",
-  option6: "Je hebt het 6e cadeautje gekozen!"
+  option1: {
+    question: 'Kies links of recht (1)',
+    nextLeft: 'option2',
+    nextRight: 'option3',
+  },
+  option2: {
+    question: 'Kies links of recht (2)',
+    bg: './../public/img/imgPage2.png',
+    nextLeft: 'option4',
+    nextRight: 'option5',
+  },
+  option3: {
+    question: 'Kies links of recht (3)',
+    bg: './../public/img/imgPage2.png',
+    nextLeft: 'option6',
+    nextRight: 'option7',
+  },
+  option4: {
+    question: 'Kies links of recht (4)',
+    bg: './../public/img/imgPage2.png',
+    nextLeft: 'option8',
+    nextRight: 'option9',
+  },
+  option5: {
+    question: 'Kies links of recht (5)',
+    bg: './../public/img/imgPage4.png',
+    nextLeft: 'optionStop',
+    nextRight: 'optionStop',
+  },
+  option6: {
+    question: 'Kies links of recht (4)',
+    bg: './../public/img/imgPage2.png',
+    nextLeft: 'option12',
+    nextRight: 'option13',
+  },
+  option7: {
+    question: 'Kies links of recht (5)',
+    bg: './../public/img/imgPage2.png',
+    nextLeft: 'option14',
+    nextRight: 'option15',
+  },
+  optionStop:{
+    question: 'Kies links of recht (stop)',
+    bg: './../public/img/imgPage3.png',
+  }
 };
 
-const selectedOption = localStorage.getItem('selectedOption');
+
+
+/*const selectedOption = localStorage.getItem('selectedOption');
 
 if (selectedOption) {
   // If the user has already selected an option, highlight the appropriate button
@@ -30,12 +84,14 @@ optionButtons.forEach(button => {
     button.classList.add('selected');
 
     // Save the selected option to local storage
-    localStorage.setItem('selectedOption', button.value);
+    localStorage.setItem(`${'selectedOption'} ${button.value}`, button.value);
 
     // Update the content with the selected option
     content.textContent = options[button.value];
+
   });
-});
+});*/
+
 
 resetButton.addEventListener('click', () => {
   // Remove all local storage items
@@ -47,4 +103,64 @@ resetButton.addEventListener('click', () => {
   // Clear the content
   content.textContent = "Je hoort uit de gang iets fluisteren naar jouw: “psst hej! Jij daar! Zie je deze cadeautjes? één van deze is voor u! Kies je het linkse cadeautje of het rechtse?";
 });
+
+// This needs to point to the web socket in the Node-RED flow
+// ... in this case it's /simple
+var wsUri = 'ws://' + '192.168.100.1:1880' + '/ws';
+
+window.onload = () => {
+  wsConnect();
+};
+
+function wsConnect() {
+  console.log('connect', wsUri);
+  var ws = new WebSocket(wsUri);
+
+
+  ws.onmessage = function (msg) {
+    //console.log(msg.data);
+    currentTime = Date.now();
+    //console.log(currentTime);
+    if (currentTime > time + 10000) {
+      console.log(currentTime);
+      console.log("ready");
+      if(options[currentOption] && options[currentOption].nextLeft){
+
+        if (msg.data === 'left') {
+          currentOption = options[currentOption].nextLeft;
+          background.style.backgroundImage = `url(${options[currentOption].bg})`;
+          content.textContent = options[currentOption].question;
+        } else if(msg.data === 'right'){
+          currentOption = options[currentOption].nextRight;
+          background.style.backgroundImage = `url(${options[currentOption].bg})`;
+          content.textContent = options[currentOption].question;
+        } else if(currentTime > time + 300000) {
+
+        }
+        time = currentTime;
+      }  
+    }  
+
+  };
+
+  ws.onopen = function () {
+    console.log('Connected');
+  };
+
+  ws.onclose = function () {
+    // in case of lost connection tries to reconnect every 3 secs
+    setTimeout(wsConnect, 3000);
+  };
+
+  ws.disconnect = function () {
+    console.log('Disconnected');
+  };
+}
+
+function update() {
+  // now send the output over the websocket
+  if (ws) {
+    ws.send(output);
+  }
+}
 
