42d14dea1f4d9dbadfb9a01202ce3419a6abe2e2
JarneBukkes <jarne.budts@hotmail.com>
2023-05-04 11:55:12 +0200

    duistersEinde Af

--- interactiveStory/src/script.js
+++ interactiveStory/src/script.js
@@ -34,10 +34,10 @@ gameshow.play();
   
   });
   var voices = window.speechSynthesis.getVoices();
-message.pitch = 1.1;
-message.volume = 1;
-message.lang = "nl-NL";
-message.rate = 1.1;
+message.pitch = 2;
+message.volume = 2;
+message.lang = "nl-BE";
+message.rate = 1.5;
 
   
 let currentOption = 'option1';
@@ -49,7 +49,7 @@ const options = {
  
   },
   option2: {
-    question: 'Hoera, je hebt een banaan gewonnen! In de andere kamer viel een vele betere prijs te winnen, ik ga je nog een kans geven om iets coolers te winnen, kies de juiste deur en win een betere prijs!',
+    question: 'Hoera, je hebt een banaan gewonnen! In de andere kamer viel wel een vele betere prijs te winnen, ik ga je nog een kans geven om iets coolers te winnen, kies de juiste deur en win een betere prijs!',
     bg: './public/img/Gameshow/Gameshow.png',
     nextLeft: 'option4',
     nextRight: 'option4',
@@ -57,8 +57,8 @@ const options = {
 
   },
   option3: {
-    question: 'PROFICIAT, u heeft een overheerlijk TAARTJE gewonnen!  In de andere kamer viel een vele betere prijs te winnen, wil je het op eten?"\nJA:LINKS\nNEE: RECHTS',
-    bg: './public/img/imgPage2.png',
+    question: 'PROFICIAT, u heeft een overheerlijk TAARTJE gewonnen! In de andere kamer viel wel een vele betere prijs te winnen, wil je het op eten? "JA:LINKS NEE: RECHTS',
+    bg: './public/img/Gameshow/Gameshow.png',
     nextLeft: 'option6',
     nextRight: 'option7',
     sound: './public/sound/klap.mp3'
@@ -77,10 +77,11 @@ const options = {
     nextRight: 'optionStop',
   },
   option6: {
-    question: '“Hmm wat een lekker taartje seg”, denk je. Maar je begint je onwel te voelen, heb je dan nooit niet geleerd om eten van een vreemde op te eten? Je valt in slaap...',
-    bg: './public/img/imgPage2.png',
+    question: 'Het was een lekker taartje maar, je begint je onwel te voelen, heb je dan nooit niet geleerd om eten van een vreemde op te eten? Je valt in slaap... Ga door een deur om voort te gaan',
+    bg: './public/img/Gameshow/gameshow_drugs.jpg',
     nextLeft: 'option12',
-    nextRight: 'option13',
+    nextRight: 'option12',
+    sound: './public/sound/snacking.mp3'
   },
   option7: {
     question: 'Wat? Waarom wil je het taartje niet eten? ik heb er zelfs nog zo veel tijd in gestoken! podium begint te schudden, gebouw begint in te storten.  en lichten beginnen te flikkeren. Waarom eet nooit iemand van mijn taartjes! Waarom! Nog eens een luide knal en het wordt plots zwart voor je ogen',
@@ -106,12 +107,59 @@ const options = {
   },
 
   option10: {
-    question: 'Wat? Waarom wil je het taartje niet eten? ik heb er zelfs nog zo veel tijd in gestoken! podium begint te schudden, gebouw begint in te storten.  en lichten beginnen te flikkeren. Waarom eet nooit iemand van mijn taartjes! Waarom! Nog eens een luide knal en het wordt plots zwart voor je ogen',
+    question: 'Wat? Waarom wil je het taartje niet eten? storten. Het podium begint in te storten en het wordt zwart voor je ogen',
     bg: './public/img/imgPage2.png',
     nextLeft: 'option14',
     nextRight: 'option15',
   },
+  option12: {
+    question: 'Je wordt wakker in een steegje, je voelt je gedesoriënteerd en verward. Je realiseert je al snel dat er iets vreselijks is gebeurd en dat je je niet kunt herinneren wat er is gebeurd nadat je het taartje hebt gegeten. Je begint te huilen en voelt je vreselijk kwetsbaar en bang. Je realiseert je dat je hulp nodig hebt en begint op zoek te gaan naar iemand die je kan helpen. Kies een deur om hulp te zoeken, kies verstandig!',
+    bg: './public/img/Crossroads/pad.png',
+    nextLeft: 'option18',
+    nextRight: 'option19',
+    sound: './public/sound/ambientBB.mp3'
+  },
+  option18: {
+    question: 'Terwijl je probeert uit te leggen wat er is gebeurd, merk je dat de mensen om je heen steeds vreemder reageren. Hun gezichten beginnen te vervormen en hun stemmen klinken onheilspellend. Je wordt bang en loopt snel de andere kant uit.',
+    bg: './public/img/Group.png',
+    nextLeft: 'option20',
+    nextRight: 'option20',
+    sound: './public/sound/laugh.mp3'
+    
+  },
+  option19: {
+    question: 'Je hebt door dat dit misschien een enorme vergissing was. De mensen waar je iets aan vraagt praten niet terug en kijken gewoon raar. Je voelt je hartslag versnellen en begint te trillen van angst en je begint te lopen voor je leven.',
+    bg: './public/img/Group.png',
+    nextLeft: 'option20',
+    nextRight: 'option20',
+    sound: './public/sound/laugh.mp3'
+  },
 
+  option20: {
+    question: 'Terwijl je aan het weglopen bent begint de wereld te vervormen, felle kleuren komen van overal en de gebouwen beginnen vreemde vormen aan te nemen. Je begint je aangenamer te voelen en je krijgt een warm gevoel over je heen',
+    bg: './public/img/HallucinationAlley.png',
+    nextLeft: 'option21',
+    nextRight: 'option21',
+    sound: './public/sound/heartbeat.mp3'
+    
+  },
+  option21: {
+    question: 'Na een tijdje begin je te beseffen dat je in een kamertje ligt, er valt in dit kamertje niet veel te zien, het is muisstil... Je hoort een stem door je hoofd gaan... LINKS of RECHTS',
+    bg: './public/img/EmptyRoom.png',
+    nextLeft: 'option22',
+    nextRight: 'option22',
+    
+  },
+
+  option22: {
+    question: 'LINKS of RECHTS',
+    bg: './public/img/EmptyRoom.png',
+    nextLeft: 'option22',
+    nextRight: 'option22',
+    
+  },
+
+  
   option14:{
     question: 'Ik weet niet hoe ik dit moet zeggen maar. Je bent weeral verkeerd... Probeer misschien nog eens?',
     bg: './public/img/Gameshow/glitchingGameshow.png',
@@ -225,10 +273,10 @@ function wsConnect() {
             message.text = options[currentOption].question;
             speechSynthesis.speak(message);
           }, 5000);
-          message.pitch = 1.1;
-          message.volume = 1;
-          message.lang = "nl-NL";
-          message.rate = 1.1;
+          message.pitch = 2;
+          message.volume = 2;
+          message.lang = "nl-BE";
+          message.rate = 1.5;
           var sound = new Audio();
           sound.src = options[currentOption].sound;
           sound.play();
@@ -245,10 +293,10 @@ function wsConnect() {
             message.text = options[currentOption].question;
             speechSynthesis.speak(message);
           }, 5200);
-          message.pitch = 1.1;
-          message.volume = 1;
-          message.lang = "nl-NL";
-          message.rate = 1.1;
+          message.pitch = 2;
+          message.volume = 2;
+          message.lang = "nl-BE";
+          message.rate = 1.5;
           var sound = new Audio();
           sound.src = options[currentOption].sound;
           sound.play();
