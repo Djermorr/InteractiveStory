f391916c6c7802e45049f84db1d800003136c213
Robbe Van Dijck <robbeelvandijck@gmail.com>
2023-05-15 22:50:21 +0200

    added summaries

--- interactiveStory/src/script.js
+++ interactiveStory/src/script.js
@@ -48,7 +48,7 @@ const options = {
     nextLeft: 'option4',
     nextRight: 'option4',
     sound: './public/sound/klap.mp3',
-    summary: 'banaan'
+    summary: 'Je koos voor de banaan'
   },
   option3: {
     question: 'Proficiat! u heeft een overheerlijk TAARTJE gewonnen! In de andere kamer viel wel een vele betere prijs te winnen, wil je het overheerlijke taartje op eten? JA:LINKS NEE: RECHTS',
@@ -56,7 +56,7 @@ const options = {
     nextLeft: 'option6',
     nextRight: 'option7',
     sound: './public/sound/klap.mp3',
-    summary: 'taartje'
+    summary: 'Je koos voor het Taartje'
   },
   option4: {
     question: 'Jammer maar helaas, geen prijs voor jouw! Je mag je banaantje wel bijhouden maar, ik ga je toch nog een kans geven om te winnen! Kies een deur en maak kans om een betere prijs te winnen!',
@@ -64,48 +64,54 @@ const options = {
     nextLeft: 'option8',
     nextRight: 'option9',
     sound: './public/sound/fail.mp3',
-    summary: 'verliezen'
+    summary: 'Je koos weeral verkeerd'
   },
   option5: {
     question: 'Kies links of recht (5)',
     bg: './public/img/imgPage4.png',
     nextLeft: 'optionSummary',
     nextRight: 'optionSummary',
+    summary: 'Je koos weeral verkeerd'
   },
   option6: {
     question: 'Het was een lekker taartje maar, je begint je onwel te voelen, heb je dan nooit niet geleerd om eten van een vreemde op te eten? Je valt in slaap... Ga door een deur om voort te gaan',
     bg: './public/img/Gameshow/gameshow_drugs.jpg',
     nextLeft: 'optionSummary',
     nextRight: 'option12',
-    sound: './public/sound/snacking.mp3'
+    sound: './public/sound/snacking.mp3',
+    summary: 'Je besloot een willekeurig taartje te eten'
   },
   option7: {
     question: 'Wat? Waarom wil je het taartje niet eten? ik heb er zelfs nog zo veel tijd in gestoken! Waarom eet nooit iemand van mijn taartjes! Je hoort nog eens een luide knal en het wordt plots zwart voor je ogen. Kies een deur om door te gaan',
     bg: './public/img/Presentator_ENG.png',
     nextLeft: 'option23',
     nextRight: 'option23',
-    sound: './public/sound/rumbling.mp3'
+    sound: './public/sound/rumbling.mp3',
+    summary: 'Je bent ondankbaar voor wat je krijgt'
   },
   option23: {
     question: 'Welkom op de echte gameshow, hier gaan we u testen door een scenario te geven. Kies verstandig, de volgende keuze van deur legt je pad vast ',
     bg: './public/img/Gameshow/gameshowMasterDarkPath.png',
     nextLeft: 'option24',
     nextRight: 'option25',
-    sound: './public/sound/gameshowBAD.mp3'
+    sound: './public/sound/gameshowBAD.mp3',
+    summary: 'Kiezen is verliezen'
   },
   option24: {
     question: 'Je komt in een neerstortend gebouw terecht, onderweg valt er nog een hoop puin naast je naar beneden op iemand naast je zijn been. Help! krijst de man uit, ik krijg de steen niet van mijn benen. Het gebouw begint steeds minder stevig te worden en je beseft dat er geen tijd is voor deze man te helpen. Help je hem? JA = LINKS NEE = RECHTS',
     bg: './public/img/crumblinghall.jpg',
     nextLeft: 'option26',
     nextRight: 'option27',
-    sound: './public/sound/rumbling.mp3'
+    sound: './public/sound/rumbling.mp3',
+    summary: 'Door jou keuzes ligt er iemand onder het puin'
   },
   option25: {
     question:'Raadsel: Wat was het ander cadeautje had je niet voor het taartje gekozen in het begin van de show? LINKS: een banaan, RECHTS: een appel ',
     bg: './public/img/Gameshow/supersayanPresentator.jpg',
     nextLeft: 'option27',
     nextRight: 'option28',
-    sound: './public/sound/mysterious.mp3'
+    sound: './public/sound/mysterious.mp3',
+    summary: ''
   },
 
   option26: {
@@ -114,12 +120,14 @@ const options = {
     sound: './public/sound/rumbling.mp3',
     nextLeft: 'option1',
     nextRight: 'option1',
+    summary: 'Je beslist de man te helpen die jij verdoemd hebt maar zelfs deze keuze had concequensies'
   },
   option27: {
     question: 'Proficiat, u heeft juist geantwoord, u krijgt de opportuniteit om verder te leven + u krijgt de banaan van ons!',
     bg: './public/img/Gameshow/supersayanPresentator.png',
     nextLeft: 'option1',
     nextRight: 'option1',
+    summary: 'We zien dat je een egoistische kant hebt'
     
   },
   option28: {
@@ -128,14 +136,15 @@ const options = {
     sound: './public/sound/fail2.mp3',
     nextLeft: 'option1',
     nextRight: 'option1',
-    
+    summary: 'We zien dat je een slecht geheugen hebt'
   },
   option8: {
     question: "Dit is spijtig genoeg niet de juiste weg, Weeral niet, probeer nog een keer, ik heb hier weer twee pakjes klaargezet, KIES VERSTANDIG en WIN JE GROTE PRIJS!",
     bg: './public/img/Gameshow/gameshow_presents_glitched_fout.png',
     nextLeft: 'option14',
     nextRight: 'option14',
-    sound: './public/sound/fail2.mp3'
+    sound: './public/sound/fail2.mp3',
+    summary: 'En je bleef maar proberen'
   },
 
   option9: {
@@ -144,7 +153,8 @@ const options = {
     nextLeft: 'option14',
     nextRight: 'option14',
     sound: './public/sound/buzzer.mp3',
-    summary: 'verliezen'
+    summary: 'verliezen',
+    summary: 'Was er wel een uitweg?'
   },
 
  
@@ -153,14 +163,16 @@ const options = {
     bg: './public/img/Crossroads/pad.png',
     nextLeft: 'option18',
     nextRight: 'option19',
-    sound: './public/sound/ambientBB.mp3'
+    sound: './public/sound/ambientBB.mp3',
+    summary: 'Je at een vreemd taartje, dat blijkbaar gedrugeerd was'
   },
   option18: {
     question: 'Terwijl je probeert uit te leggen wat er is gebeurd, merk je dat de mensen om je heen steeds vreemder reageren. Hun gezichten beginnen te vervormen en hun stemmen klinken onheilspellend. Je wordt bang en loopt snel de andere kant uit.',
     bg: './public/img/Group.png',
     nextLeft: 'option20',
     nextRight: 'option20',
-    sound: './public/sound/laugh.mp3'
+    sound: './public/sound/laugh.mp3',
+    summary: 'Niet iedereen is te vertrouwen natuurlijk'
     
   },
   option19: {
@@ -168,7 +180,8 @@ const options = {
     bg: './public/img/Group.png',
     nextLeft: 'option20',
     nextRight: 'option20',
-    sound: './public/sound/laugh.mp3'
+    sound: './public/sound/laugh.mp3',
+    summary: 'Hopelijk was je snel genoeg om weg te lopen'
   },
 
   option20: {
@@ -176,7 +189,8 @@ const options = {
     bg: './public/img/HallucinationAlley.png',
     nextLeft: 'option21',
     nextRight: 'option21',
-    sound: './public/sound/heartbeat.mp3'
+    sound: './public/sound/heartbeat.mp3',
+    summary: 'De drugs namen nog altijd effect op je'
     
   },
   option21: {
@@ -184,6 +198,7 @@ const options = {
     bg: './public/img/EmptyRoom.png',
     nextLeft: 'option22',
     nextRight: 'option22',
+    summary: 'Links of rechts?'
     
   },
 
@@ -192,6 +207,7 @@ const options = {
     bg: './public/img/EmptyRoom.png',
     nextLeft: 'option30',
     nextRight: 'option30',
+    summary: 'Links of rechts?'
     
   },
 
@@ -200,6 +216,7 @@ const options = {
     bg: './public/img/EmptyRoom.png',
     nextLeft: 'option1',
     nextRight: 'option1',
+    summary: 'Had je maar nooit dat taartje gegeten'
     
   },
 
@@ -210,21 +227,23 @@ const options = {
     nextLeft: 'option15',
     nextRight: 'option15',
     sound: './public/sound/bwabwa.mp3',
-    summary: 'verliezen'
+    summary: 'verliezen, verliezen, verliezen'
   },
   option15:{
     question: 'Wat is er aan het gebeuren, er wordt hier maar geen prijs gewonnen. Ik begin mezelf af te vragen of er ooit wel een prijs was? Misschien moet jij dat ook eens doen, probeer nog maar eens voor de zekerheid. Kies een deur',
     bg: './public/img/Gameshow/gameshow_presents_bannanaBAD.png',
     nextLeft: 'option16',
     nextRight: 'option16',
-    sound: './public/sound/bwabwa.mp3'
+    sound: './public/sound/bwabwa.mp3',
+    summary: 'Je begon het spel in vraag te trekken'
   },
   option16:{
     question: 'Weeral niet gewonnen! Ik geef het alvast op, jij kunt doen wat je wil, kies een deur of niet, ik ben naar huis, het interesseert mij niet meer, doei!',
     bg: './public/img/Gameshow/gameshow_presents_glitched_fout_presentatorweg.png',
     nextLeft: 'option17',
     nextRight: 'option17',
-    sound: './public/sound/bwabwa.mp3'
+    sound: './public/sound/bwabwa.mp3',
+    summary: 'En je bracht de verteller tot hun breekpunt'
   },
 
   option17:{
@@ -232,7 +251,8 @@ const options = {
     bg: './public/img/Gameshow/overgrownGameshow.png',
     nextLeft: 'option1',
     nextRight: 'option1',
-    sound: './public/sound/crickets.mp3'
+    sound: './public/sound/crickets.mp3',
+    summary: '...'
   },
 
   optionSummary:{
@@ -314,9 +334,8 @@ function wsConnect() {
           sound.src = options[currentOption].sound;
           sound.play();
         }  else if(currentTime > time + 300000) {
-
+          time = currentTime;
         }
-        time = currentTime;
       } else if(currentOption === optionSummary){
         content.textContent = options[currentOption].question;
         const currentSummary = JSON.parse(localStorage.getItem(`summary`));
